6daaba08b3a599f3e4907a2ae08ccc4c79a4e6da
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-04-15 19:03:31 +0200

    Added error class and updated error function

--- papagei.py
+++ papagei.py
@@ -5,6 +5,7 @@ from enum import IntEnum
 
 
 class AutoNumber(IntEnum):
+    """ Super class for auto numbering the Enumeration"""
     def __new__(cls):
         value = len(cls.__members__) + 1
         obj = int.__new__(cls)
@@ -13,20 +14,34 @@ class AutoNumber(IntEnum):
 
 
 class VerboseLevel(AutoNumber):
+    """ Used to check on the different verbose levels. Auto-numbered for easy changes"""
     SILENT = ()
-    ERROR =()
+    ERROR = ()
     WARNING = ()
     INFO = ()
     DEBUG = ()
     FRIVOLOUS = ()
 
 
-VERBOSE = VerboseLevel.FRIVOLOUS
+class PapageiError(Exception):
+    """ Error class to return actual errors"""
+    def __init__(self, args):
+        # Formats the arguments into a string for display (takes a tuple as input)
+        self.msg = ''
+        for arg in args:
+            self.msg += str(arg)+' '
+        self.msg = self.msg[:-1]  # Remove last space
+
+    def __str__(self):
+        return self.msg
+
+
+VERBOSE = VerboseLevel.FRIVOLOUS  # Change this variable to change the verbose level
 
 
 def error(*args):
     if VERBOSE.value <= VerboseLevel.ERROR.value:
-        print(*args)
+        raise PapageiError(args)
 
 
 def mock_error(*args):
