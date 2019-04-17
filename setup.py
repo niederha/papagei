3e607a7c8f3fccbd42eb2ab405ea1cfd6b95509e
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-04-17 13:00:47 +0200

    Corrected versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="papagei-niederha",
-    version="0.0.1",
+    version="0.0.2",
     author="Niederhauser Loïc",
     author_email="loic.niederhauser@gmail.com",
     description="A package for easy verbose logging, error and warnings.",
